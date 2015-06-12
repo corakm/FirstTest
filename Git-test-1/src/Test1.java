80ecf676b3f851ae210066eae96dc7bb849afb15
corakm <milos.corak@powervation>
2015-06-12 03:13:01 +0100

    Ovo je drugi komit sa branch-om

--- Git-test-1/src/Test1.java
+++ Git-test-1/src/Test1.java
@@ -5,5 +5,8 @@ public class Test1 {
 		// ovo je prvi test za github 1
 		
 		//Imam izmene na brancu i vratio sam se na master i dodao tekst. sad cemo da vidimo sta ce da pude kad ih spojim.
+		//24
+		//34
+		//The original file 'Test1.java' has been deleted or is not accessible.
 	}
 }
